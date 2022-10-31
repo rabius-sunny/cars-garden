219d31ad14c0f699c1aaedd7659c622960fc6d15
rabius-sunny <rabiussunnyc3x@gmail.com>
2022-10-31 12:57:09 +0600

    [FIXED] axios instance

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseURL = process.env.REACT_APP_API_BASE_URL
+// const baseURL = process.env.REACT_APP_API_BASE_URL
+const baseURL = 'https://datacenter.darulislam.foundation/sunny'
 const instance = axios.create({
   baseURL,
   timeout: 15000
